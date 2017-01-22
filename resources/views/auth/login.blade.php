fe37ad1ca8ff5aa1459e85492983895a7e52decf
mvsrakeshkumarreddy <mvsrakeshkumarreddy@gmail.com>
2017-01-23 00:22:08 +0530

    all

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.master')
+
+@section('content')
+
+<h2>Login</h2>
+@if(count($errors))
+<div class="alert alert-danger">
+	<ul>
+		@foreach($errors->all() as $error)
+		<li>  {{ $error  }}  </li>
+		@endforeach
+	</ul>
+</div>
+@endif
+echo Form::text('username');
+{!! Form::open(array('route'=>'handleLogin'))!!}
+<div class="form-group">
+	{!! Form::label('email')!!}
+	{!! Form::text('email',null,array('class'=>'form-control'))!!}
+</div>
+<div class="form-group">
+	{!! Form::label('password')!!}
+	{!! Form::password('password',array('class'=>'form-control'))!!}
+</div>
+
+{!! Form::token()!!}
+{!!Form::submit(null,array('class'=>'btn btn-default'))!!}
+{!!Form::close()!!}
+@endsection
